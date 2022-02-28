316c1a48b94a63a583546cd89ab5eb63b3025c2e
Rohit Dandnayak <rohit.dandnayak@soroco.com>
2022-02-28 08:42:45 +0530

    fix: keymap changed for git files

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -71,5 +71,5 @@ keymap("v", "p", '"_dP', opts)
 -- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
 -- keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
-keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
+keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.git_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
 keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
