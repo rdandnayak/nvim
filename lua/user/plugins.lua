e86ea433f7e095ff6d1e8df36b262b750e935e67
Rohit Dandnayak <rohit.dandnayak@soroco.com>
2022-03-22 10:20:02 +0530

    fix: taskwiki added

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -102,6 +102,7 @@ return packer.startup(function(use)
   -- Git
   use "lewis6991/gitsigns.nvim"
   use "vimwiki/vimwiki"
+  use "tools-life/taskwiki"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
---
+++ lua/user/vimwiki.lua
@@ -0,0 +1,4 @@
+vim.cmd [[
+let g:vimwiki_list = [{'auto_diary_index': 1}]
+let g:vimwiki_list = [{'path': '~/vimwiki/', 'syntax': 'markdown', 'ext': '.md'}]
+]]
