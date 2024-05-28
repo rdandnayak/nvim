4b14f6fc5d5fde36da6235223e09666a04bd5aef
Rohit Dandnayak <rohit.dandnayak@soroco.com>
2024-05-28 15:39:01 +0530

    refresh of pacakges

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -52,8 +52,7 @@ return packer.startup(function(use)
   use { "kyazdani42/nvim-tree.lua", 
     requires = {
       'kyazdani42/nvim-web-devicons', -- optional, for file icon
-    }, 
-    tag="nightly"
+    }  
   }
   -- use "moll/vim-bbye"
   -- use "nvim-lualine/lualine.nvim"
